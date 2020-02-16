ae30daf69ddb2f4ffa05d39d4fb0cc141a37ced0
Lix Luthor <689071+rezemble@users.noreply.github.com>
2020-02-16 20:10:03 +0100

    improve express api support

--- README.md
+++ README.md
@@ -34,8 +34,37 @@ app.use(async (req, res) => {
 app.listen(8080)
 ```
 
+### usage with express-style error handler
+```js
+const express = require('express')
+const app = express()
+
+const geh = require('express-global-error-handler')
+
+// use the default handler to forward all errors (including promise rejections)
+geh(app)
+
+// produce some errors:
+app.use(async (req, res) => {
+	// foo is undefined - will throw ReferenceError
+	res.send(foo())
+})
+
+// the express default error handler will receive all async errors
+app.use((err, req, res, next) => {
+	console.error('some error')
+	res.status(500).send({
+		error: err.message,
+		stack: err.stack,
+	})
+})
+
+app.listen(8080)
+```
+
+
 ### api
-`(app: express, handler: function, modes?: <String>)`
+`(app: express, handler: function?, modes?: <String>)`
 - `app` is the express instance to use (or a `router`)
-- `handler` is the error handling function
-- `mode` is an optional array of express methods to override (defaults to `['get', 'post', 'put', 'delete', 'use']`)
+- `handler` is the error handling function (defaults to `(err, req, res, next) => next(err)`)
+- `modes` is an optional array of express methods to override (defaults to `['all', 'get', 'post', 'put', 'delete', 'patch', 'options', 'head', 'use']`)
--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-module.exports = (app, handler, modes = ['get', 'post', 'put', 'delete', 'use']) => {
-  const raw = Object.fromEntries(modes.map(key => [key, app[key].bind(app)]))
-  for (const [key, f] of Object.entries(raw)) {
-    app[key] = (...args) => {
-      if (args.length === 2) {
-        const [route, f] = args
-        raw[key](route, async (...args) => {
-          if (args.length > 3) return f(...args)
-          const [req, res, next] = args
-          try {
-            await f(...args)
-          } catch(e) {
-            return handler(e, req, res, next)
-          }
-        })
-      } else {
-        const [f] = args
-        raw[key](async (...args) => {
-          if (args.length > 3) return f(...args)
-          const [req, res, next] = args
-          try {
-            await f(...args)
-          } catch(e) {
-            return handler(e, req, res, next)
-          }
-        })
-      }
+module.exports = (
+  app,
+  handler = (err, req, res, next) => next(err),
+  modes = ['all', 'get', 'post', 'put', 'delete', 'patch', 'options', 'head', 'use']
+) => modes.map(key => [key, app[key].bind(app)]).forEach(
+  ([key, orig]) => app[key] = (...args) => orig(...args.map(arg => typeof arg === 'function' ? arg.length === 4 ? arg : async (...argv) => {
+    try {
+      await arg(...argv)
+    } catch(error) {
+      return handler(error, ...argv)
     }
-  }
-}
+  } : arg)))
